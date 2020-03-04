bd8f96a3904267e83208a2aeee8dc1932a58976a
SemenAndronaki <semenandronaki@gmail.com>
2020-03-05 01:52:10 +0300

    tests for Point added

---
+++ sandbox/src/test/java/distance/DistanceCalculatorTest.java
@@ -0,0 +1,26 @@
+package distance;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DistanceCalculatorTest {
+
+    @Test
+    public void testDistance0(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(-1, 0);
+        Assert.assertEquals(p1.distance(p2), 1.0, "Wrong distance");
+    }
+
+    @Test
+    public void testDistance1(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+        Assert.assertEquals(p1.distance(p2), 5.0, "Wrong distance");
+    }
+
+    @Test
+    public void testDistance2(){
+        Assert.assertEquals(new Point(0, 0).distance(new Point(0, 0)), 0.0, "Wrong distance");
+    }
+}
