2e544c53c682354d1fe1e3261a424d6a5bad17b6
SemenAndronaki <semenandronaki@gmail.com>
2020-04-05 03:03:03 +0300

    DB Helper added

--- addressbook-web-tests/src/test/java/ru/addressbook/appManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appManager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
     private String browser;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -30,6 +31,9 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper = new DbHelper();
+
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -59,5 +63,9 @@ public class ApplicationManager {
 
     public ContactHelper getContactHelper() {
         return contactHelper;
+    }
+
+    public DbHelper getDbHelper() {
+        return dbHelper;
     }
 }
