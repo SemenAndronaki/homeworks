607da83992e09aa71b3054f33f41bb1ba0fb1fca
SemenAndronaki <semenandronaki@gmail.com>
2020-04-11 03:59:57 +0300

    added registration and wd lazy load

---
+++ mantiss-tests/src/test/java/ru/mantiss/tests/RegistrationTests.java
@@ -0,0 +1,11 @@
+package ru.mantiss.tests;
+
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+    @Test
+    public void testRegistration() {
+        app.registration().start("user1", "user1@test.com");
+    }
+}
