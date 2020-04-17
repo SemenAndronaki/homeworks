a840d572c226a47a49a20e79f6faacc97c4ddadc
SemenAndronaki <semenandronaki@gmail.com>
2020-04-17 11:13:02 +0300

    fix distance tests

--- sandbox/src/test/java/distance/DistanceCalculatorTest.java
+++ sandbox/src/test/java/distance/DistanceCalculatorTest.java
@@ -16,7 +16,7 @@ public class DistanceCalculatorTest {
     public void testDistance1(){
         Point p1 = new Point(0, 0);
         Point p2 = new Point(3, 4);
-        Assert.assertEquals(p1.distance(p2), 2.0, "Wrong distance");
+        Assert.assertEquals(p1.distance(p2), 5.0, "Wrong distance");
     }
 
     @Test
